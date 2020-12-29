a7852a8103641e696d0f0524bf16cd93445972e8
jwcub <jwcub@outlook.com>
2020-12-29 17:39:10 +0800

    添加更多命令

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import threading
 import time
 import copy
 from time import sleep
+import requests
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -13,6 +14,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+import json
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.nlp.v20190408 import nlp_client, models
+from PIL import Image
+import sys
+import os
 
 
 class Bot(object):
@@ -21,10 +31,14 @@ class Bot(object):
     https://www.luogu.com.cn/paste/nbyi7ds9
     """
 
-    def __init__(self, username, password, roomId, isSecret=False, isAutoReady=True):
+    def __init__(self, username, password, roomId, isSecret, controller, mode, isAutoReady=True):
         self.kanaLink = "https://kana.byha.top:444/"
-        self.driver = webdriver.Chrome()  # 浏览器
-        # self.driver = webdriver.Firefox()
+        self.mode = int(mode)
+        option = webdriver.ChromeOptions()
+        if self.mode == 2:
+            option.add_argument("headless")
+        self.driver = webdriver.Chrome(options=option)  # 浏览器
+        # self.driver = webver.Firefox()
         self.username = username  # 用户名
         self.password = password  # 密码
         self.roomId = roomId  # 房间号
@@ -48,7 +62,18 @@ class Bot(object):
         self.freeTime = 0  # 空闲时间
         self.TIME_PER_TURN = 0.24  # 每回合的等待时间
         self.msg = []  # 消息区
-        self.waittime = {} # 等待时间
+        self.waittime = {}  # 等待时间
+        self.tips = ['输入help以查看命令帮助', '命令refresh可以使Bot强制刷新', '命令info可以获取Bot的工作信息', 'Bot的扩张优先级：主基地>城市>敌方领地>空白土地>我方领地',
+                     'Bot在每回合有1/5的概率发起掏家', '若自身主基地受到威胁，Bot会优先防守', 'Bot在每回合有1/15的概率发起随机扩张', 'Bot更倾向于选取靠外的格子进行扩张'
+            , 'Bot的随机扩张目标是周围7*7范围内有不少于5个敌方领地的格子', '命令attack [x] [y]可以使Bot攻击第x行第y列的格子', '命令chat [xxx]可以与Bot对话']
+        self.gameCnt = 0
+        self.startTime = time.time()
+        self.controller = controller
+        self.gameStartTime = time.time()
+        fo = open('secret')
+        s = fo.readlines()
+        self.SecretId = s[0].strip()
+        self.SecretKey = s[1].strip()
 
     def SendKeyToTable(self, key):
         ac = ActionChains(self.driver)
@@ -147,11 +172,13 @@ class Bot(object):
                 break
         return
 
-    def UpdateWaitTime(self, uname):
+    def UpdateWaitTime(self, uname, t = 60):
+        if uname == self.controller:
+            return True
         try:
-            if time.time() - self.waittime[uname] < 60:
+            if time.time() - self.waittime[uname] < t:
                 self.sendMessage(
-                    "@" + uname + " please try again in " + str(60 - time.time() + self.waittime[uname]) + "s")
+                    "@" + uname + " please try again in " + str(t - time.time() + self.waittime[uname]) + "s")
                 return False
             self.waittime[uname] = time.time()
             return True
@@ -162,10 +189,96 @@ class Bot(object):
     def CommandLine(self):  # 命令行
         self.GetMessage()
         cur = self.msg[len(self.msg) - 1]
-        if cur[1] == 'refresh':
+        tmp = ['']
+        tot = 0
+        for i in range(0, len(cur[1])):
+            if cur[1][i] == ' ' and (i == 0 or cur[1][i - 1] != ' '):
+                tot += 1
+                tmp.append('')
+            elif cur[1][i] != ' ':
+                tmp[tot] += cur[1][i]
+        if tmp[0] == 'refresh':
             if self.UpdateWaitTime(cur[0]):
+                if time.time() - self.gameStartTime < 300:
+                    self.sendMessage("游戏开始后5分钟才能使用刷新")
+                else:
+                    self.EnterRoom()
+                    print("refreshed by " + cur[0])
+        if tmp[0] == 'help':
+            self.sendMessage('命令帮助：<br>refresh：强制刷新<br>info：获取工作信息<br>attack [x] [y]: 攻击第x行第y列的格子<br>chat [xxx]: 与Bot对话')
+        if tmp[0] == 'info':
+            if self.UpdateWaitTime(cur[0]):
+                self.sendMessage(
+                    'Bot工作状态：<br>已运行' + str(time.time() - self.startTime) + 's<br>' + '参战' + str(self.gameCnt) + '局')
+        if tmp[0] == 'attack':
+            if self.UpdateWaitTime(cur[0]):
+                if tot != 2:
+                    self.sendMessage('需要2个参数，发现' + str(tot) + '个')
+                elif self.driver.find_element_by_id("game-status").get_attribute('innerHTML') != "游戏中":
+                    self.sendMessage('不在游戏中')
+                else:
+                    tryTime = 0
+                    while True:
+                        self.ChangeTarget()
+                        x = self.q[0][0]
+                        y = self.q[0][1]
+                        tryTime += 1
+                        self.q.pop(0)
+                        if not (self.mpTmp[x][y] <= 1 and self.mpType[x][y] != 2 and tryTime <= 10):
+                            break
+                    try:
+                        ex = int(tmp[1])
+                        ey = int(tmp[2])
+                        if 1 <= ex <= self.size and 1 <= ey <= self.size:
+                            self.sendMessage('(' + str(x) + ', ' + str(y) + ') -> (' + str(ex) + ', ' + str(ey) + ')')
+                            self.Attack(x, y, ex, ey)
+                            self.sendMessage(str(self.ansLen))
+                        else:
+                            self.sendMessage('参数不在范围内')
+                    except:
+                        self.sendMessage('参数应为整数')
+        if tmp[0] == 'chat':
+            if self.UpdateWaitTime(cur[0], 5):
+                if tot != 1:
+                    self.sendMessage('需要1个参数，发现' + str(tot) + '个')
+                else:
+                    try:
+                        cred = credential.Credential(self.SecretId, self.SecretKey)
+                        httpProfile = HttpProfile()
+                        httpProfile.endpoint = "nlp.tencentcloudapi.com"
+                        clientProfile = ClientProfile()
+                        clientProfile.httpProfile = httpProfile
+                        client = nlp_client.NlpClient(cred, "ap-guangzhou", clientProfile)
+
+                        req = models.ChatBotRequest()
+                        params = {
+                            "Query": tmp[1]
+                        }
+                        req.from_json_string(json.dumps(params))
+
+                        resp = client.ChatBot(req)
+                        s = resp.Reply
+                        self.sendMessage(s)
+
+                    except TencentCloudSDKException as err:
+                        print(err)
+        if tmp[0] == 'kill':
+            if cur[0] == self.controller:
+                self.Kill()
+            else:
+                self.sendMessage('权限不足')
+        if tmp[0] == 'enter':
+            if cur[0] == self.controller:
+                self.roomId = tmp[1]
                 self.EnterRoom()
-                print("refreshed by " + cur[0])
+            else:
+                self.sendMessage('权限不足')
+        if tmp[0] == 'setsecret':
+            if cur[0] == self.controller:
+                self.isSecret = not self.isSecret
+                self.sendMessage('secret = ' + str(self.isSecret))
+            else:
+                self.sendMessage('权限不足')
         return
 
     def SelectLand(self, x, y):  # 选择土地
@@ -177,6 +290,9 @@ class Bot(object):
             return
 
     def sendMessage(self, msg):  # 发送消息
+        if len(msg) >= 92:
+            self.sendMessage(msg[0:90])
+            self.sendMessage(msg[90:len(msg)])
         messageBox = self.driver.find_element_by_id("msg-sender")
         ac = ActionChains(self.driver)
         ac.send_keys_to_element(messageBox, msg)
@@ -192,12 +308,36 @@ class Bot(object):
         self.driver.get(self.kanaLink)
         usernameBox = self.driver.find_element_by_name("username")
         passwordBox = self.driver.find_element_by_name("pwd")
+        capBox = self.driver.find_element_by_name("cap")
         ac = ActionChains(self.driver)
 
         # 输入账号密码并登录
         ac.send_keys_to_element(usernameBox, self.username)
         ac.send_keys_to_element(passwordBox, self.password)
-        sleep(10)  # 等待用户手动输入验证码
+        if self.mode == 2:
+            self.driver.get_screenshot_as_file('tmp.png')
+            img = Image.open('tmp.png')  # 获取图片对象
+            width = img.width  # 获取图片宽度
+            height = img.height  # 获取图片高度
+            # https://blog.csdn.net/qq_19655383/article/details/70176349
+            gray_img = img.convert('L')  # 图片转换为'L'模式  模式“L”为灰色图像，它的每个像素用8个bit表示，0表示黑，255表示白，其他数字表示不同的灰度
+
+            scale = width // width  # 图片缩放100长度
+            char_lst = ' .:-=+*#%@&?'  # 要替换的字符
+            char_len = len(char_lst)  # 替换字符的长度
+
+            for y in range(0, height, scale):  # 根据缩放长度 遍历高度
+                for x in range(0, width, scale):  # 根据缩放长度 遍历宽度
+                    choice = gray_img.getpixel((x, y)) * char_len // 255  # 获取每个点的灰度  根据不同的灰度填写相应的 替换字符
+                    if choice == char_len:
+                        choice = char_len - 1
+                    sys.stdout.write(char_lst[choice])  # 写入控制台
+                sys.stdout.write('\n')
+                sys.stdout.flush()
+            sleep(30)
+            ac.send_keys_to_element(capBox, input())
+        else:
+            sleep(10)  # 等待用户手动输入验证码
         ac.click(self.driver.find_element_by_id("submitButton")).perform()
 
         try:
@@ -468,6 +608,7 @@ class Bot(object):
         self.EnterRoom()
         self.freeTime = 0
         self.table = self.driver.find_element_by_tag_name("tbody")
+        flag = False
         while True:
             if self.driver.current_url == "https://kana.byha.top:444/":
                 self.EnterRoom()
@@ -485,9 +626,12 @@ class Bot(object):
             self.GetMap()
             self.freeTime += 1
             # print(self.freeTime)
-            if self.freeTime % 240 == 239 and not self.isSecret:
-                self.sendMessage(
-                    '欢迎来<a href="' + "https://kana.byha.top:444/checkmate/room/" + self.roomId + '">' + self.roomId + '</a>玩')
+            sleep(0.2)
+            self.CommandLine()
+            if self.freeTime % 480 == 10 and not self.isSecret:
+                self.sendMessage("【提示】" + self.tips[random.randint(0, len(self.tips) - 1)])
+                # self.sendMessage(
+                #   '欢迎来<a href="' + "https://kana.byha.top:444/checkmate/room/" + self.roomId + '">' + self.roomId + '</a>玩')
             try:
                 checkBox = self.driver.find_element_by_class_name("form-check-input")  # 防私密
                 if (checkBox.is_selected() and not self.isSecret) or (not (checkBox.is_selected()) and self.isSecret):
@@ -498,7 +642,12 @@ class Bot(object):
                 pass
             try:
                 if self.driver.find_element_by_id("game-status").get_attribute('innerHTML') != "游戏中":
+                    self.gameStartTime = time.time()
+                    if flag:
+                        self.gameCnt += 1
+                        flag = False
                     continue
+                flag = True
             except:
                 continue
             self.sx = 0
@@ -523,5 +672,9 @@ print("输入房间号：")
 t3 = input()
 print("是否私密？(Y/N)")
 t4 = input()
-a = Bot(t1, t2, t3, t4 == "Y")
+print("输入控制者：")
+t5 = input()
+print("输入模式：（1=图形化模式，2=命令行模式）")
+t6 = input()
+a = Bot(t1, t2, t3, t4 == "Y", t5, t6)
 a.Main()
